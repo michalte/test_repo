b26b48bcc474c96ad151ff5a3d33adc702df1a64
Michał <michal.tecza97@gmail.com>
2020-03-25 14:41:58 +0100

    "beautiful world"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to my world!");
+        System.out.println("The world is so obviously beautiful!!");
     }
 }
