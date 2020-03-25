0ff64c9244d8a774380814384b22658b154f85e9
Michał <michal.tecza97@gmail.com>
2020-03-25 14:43:19 +0100

    "3 times sout"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to my world!");
+        System.out.println("No way");
+        System.out.println("Cmon");
     }
 }
